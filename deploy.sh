0de1617f2b4ab123e54d85672d88c09580d76211
Peter <tp6u035@gmail.com>
2022-06-27 14:16:07 +0800

    change repo name

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f https://github.com/PeterChennn/peter-coolblog.git master:gh-pages
+git push -f https://github.com/PeterChennn/peter-blog.git master:gh-pages
 
 cd -
